0b4955345be3f5370fd4f2485604a070cb6a0f11
Barbariansyah <bariansyahi@gmail.com>
2020-09-23 00:59:37 +0700

    get sizes

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from steganography.cipher.vigenere import extended_vigenere_encrypter
+from steganography.image_steganography.lsb import *
+from steganography.util import *
 
 if __name__ == "__main__":
-    res = extended_vigenere_encrypter('plain teks', 'bari')
-    print(format(res[0], '08b'))
+    '''
+    testing embed
+    '''
+    embed_to_image(embedded_file="steganography/sample_images/small-kanye.png", cover_file="steganography/sample_images/shorthair.png", key="steganography", method="lsb", encrypt=True, sequential=True)
+
+
+    '''
+    testing utils
+    '''
+    # print(get_file_size("steganography/sample_images/small-shorthair.png"))
+
+    # res = extended_vigenere_encrypter(b'plain teks', 'bari')
+    # print(res)
+    # print(format(res[0], '08b'))
