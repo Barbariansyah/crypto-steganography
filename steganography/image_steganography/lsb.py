c53791be3be445837e155c9709db74f4d819318b
Barbariansyah <bariansyahi@gmail.com>
2020-09-23 02:49:15 +0700

    first metadata to binary init

--- steganography/image_steganography/lsb.py
+++ steganography/image_steganography/lsb.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from pathlib import Path
-from ..util import seed_generator, calculate_image_capacity, get_file_size
+from ..util import seed_generator, calculate_image_capacity, get_file_size, image_metadata_to_binary
 
 resource_path = Path('././')
 destination_path = Path('././steganography/sample-result')
@@ -12,11 +12,24 @@ pixel contains r, g, b, a value in integer
 def embed_to_image_lsb():
     pass
 
+def embed_to_image_bpcs():
+    pass
+
 def embed_to_image(embedded_file: str, cover_file: str, key: str, method: str, encrypt: bool, sequential: bool, threshold: bool = 0.3):  
-    print(get_file_size(embedded_file))
+    embedded_file_size = get_file_size(embedded_file)
+    
     with Image.open(resource_path/cover_file) as img:
         width, height = img.size
-        print(calculate_image_capacity(width, height))
+        cover_capacity_bit, cover_capacity_byte = calculate_image_capacity(width, height)
+        
+        if(embedded_file_size > cover_capacity_byte):
+            return 'embedded file size is too big for cover capacity'
+        else:
+            if(method=='lsb'):
+                embed_to_image_lsb()
+            else:
+                embed_to_image_bpcs()
+
 
 def extract_from_image():
     with Image.open(resource_path/"small-shorthair.png") as img:
--- steganography/util.py
+++ steganography/util.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import struct
 
 root_path = Path('./')
 
@@ -23,5 +24,23 @@ def handle_ascii_file(file):
 
 def get_file_size(file_name):
     return os.stat(root_path/file_name).st_size
-    # print(root_path/file_name)
-    # return(root_path)
+
+def float_to_binary(float_input):
+    return bin(struct.unpack('!I', struct.pack('!f', float_input))[0])[2:].zfill(32)
+
+def binary_to_float(binary):
+    return struct.unpack('!f',struct.pack('!I', int(binary, 2)))[0]
+
+def image_metadata_to_binary(method, encrypt, sequential, seed, threshold, file_name):
+    metadata_binary = ''
+    metadata_binary += '0' if method == 'lsb' else '1'
+    metadata_binary += '0' if encrypt == False else '1'
+    metadata_binary += '0' if sequential == False else '1'
+    metadata_binary += format(seed, '08b')
+    
+    file_name_binary = ''.join(format(ord(c), '08b') for c in file_name)
+    metadata_binary += format(len(file_name_binary), '016b')
+    metadata_binary += file_name_binary
+    return metadata_binary
+
+        
