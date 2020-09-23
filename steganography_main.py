b81de7f79bf8d867bf74a0b737471c95d4a4f043
Barbariansyah <bariansyahi@gmail.com>
2020-09-23 18:26:22 +0700

    add lsb and bpcs

--- steganography_main.py
+++ steganography_main.py
@@ -4,10 +4,10 @@ from steganography.image_steganography.lsb import *
 from steganography.util import *
 
 if __name__ == "__main__":
-    metadata_binary = image_metadata_to_binary(method='lsb', encrypt=False, sequential=True, threshold=0.3, file_size=2000, file_name='test.png')
-    print(metadata_binary)
+    # metadata_binary = image_metadata_to_binary(method='lsb', encrypt=False, sequential=True, threshold=0.3, file_size=2000, file_name='test.png')
+    # print(metadata_binary)
     # print(binary_to_float('00111110100110011001100110011010'))
     # print(binary_to_int('00000000000000000000011111010000'))
     # print(binary_to_string('0111010001100101011100110111010000101110011100000110111001100111'))
-    # embed_to_image(embedded_file="steganography/sample_images/small-kanye.png", cover_file="steganography/sample_images/shorthair.png", key="steganography", method="lsb", encrypt=True, sequential=True)
+    embed_to_image(embedded_file="steganography/sample_images/small-kanye.png", cover_file="steganography/sample_images/shorthair.png", key="steganography", method="lsb", encrypt=True, sequential=True)
     pass
