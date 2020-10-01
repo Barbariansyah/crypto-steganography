f0822b43b672613426e701d115e7e382fc3bb5ed
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-10-01 21:28:02 +0700

    add play audio

--- steganography/__init__.py
+++ steganography/__init__.py
@@ -1,8 +1,9 @@
 from steganography.image_steganography import embed_to_image, extract_from_image, save_image
-from steganography.wav_steganography import embed_to_audio, extract_from_audio, save_audio
+from steganography.wav_steganography import embed_to_audio, extract_from_audio, save_audio, play_audio
 from steganography.video_steganography import embed_to_video, extract_from_video, save_video, play_video
 from steganography.util import save_bytes_to_file
 
 __all__ = ['save_bytes_to_file', 'embed_to_image', 'extract_from_image',
            'save_image', 'embed_to_audio', 'extract_from_audio', 'save_audio',
-           'embed_to_video', 'extract_from_video', 'save_video', 'play_video']
+           'embed_to_video', 'extract_from_video', 'save_video', 'play_video',
+           'play_audio']
